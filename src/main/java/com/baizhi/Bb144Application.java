f41df489cb275fe3df94180576446499107b7593
shenyou <iiiolll@163.com>
2019-03-13 19:49:58 +0800

    111

---
+++ src/main/java/com/baizhi/Bb144Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bb144Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bb144Application.class, args);
+    }
+
+}
